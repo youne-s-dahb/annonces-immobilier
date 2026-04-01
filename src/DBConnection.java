6e48296958d7170e9d752ed22ff8b95a22f27f22
Youne-s-dahbi <dahyoubi123@gmail.com>
2026-04-01 22:18:12 +0100

    first commit

---
+++ src/DBConnection.java
@@ -0,0 +1,23 @@
+import java.sql.*;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class DBConnection {
+    public static Connection getConnection(){
+
+        try {
+
+            Dotenv dotenv = Dotenv.load();
+
+            String url = dotenv.get("DB_URL");
+            String user = dotenv.get("DB_USER");
+            String pass = dotenv.get("DB_PASSWORD");
+
+            return DriverManager.getConnection(url, user, pass);
+
+
+        } catch (Exception e) {
+            System.out.println("Erreur: " + e.getMessage());
+            return null;
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Connection con = DBConnection.getConnection();
+
+        if (con != null) {
+            System.out.println("Connexion réussie !");
+        } else {
+            System.out.println("Connexion échouée !");
+
+        }
+    }
+}
