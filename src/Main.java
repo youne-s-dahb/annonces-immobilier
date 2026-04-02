e0674b28f4f3461a6f79fb158a08bc9204cc39b9
lina <kamadlina12@gmail.com>
2026-04-02 01:11:04 +0100

    test3

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Connection con = DBConnection.getConnection();
 
         if (con != null) {
-            System.out.println("Connexion réussie tres biennnnn  !");
+            System.out.println("Connexion réussie mriiigla  !");
         } else {
             System.out.println("Connexion échouée🙁 !");
 
