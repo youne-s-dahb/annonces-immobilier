7d23e54b246cdf5522fe54d0c9e9d1aae07f2bd8
Youne-s-dahbi <dahyoubi123@gmail.com>
2026-04-04 11:40:43 +0100

    ajouter Interface (AnnonceService) + class (Annonce)

---
+++ src/AnnonceService.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public interface AnnonceService {
+
+
+    // Kat-rj3 lina list dyal l-annonces 3la hsab criteria (prix, category...)
+    List<Annonce> filtrer_annonces(String critere, Object valeur);
+
+    // Kat-khlina n-choufo l-detail dyal annonce wa7da b-l'id dyalha
+    Annonce consulter_annonce(int id_annonce);
+
+    // Kat-bhét b-l-kalimat l-mifta7iya (keyword) f l-cinwan aw l-wasf
+    List<Annonce> chercher_annonces(String keyword);
+
+}
