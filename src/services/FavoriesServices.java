b2d2a4b38582107bed629f900537fd0d0225a172
Youne-s-dahbi <dahyoubi123@gmail.com>
2026-04-05 15:34:47 +0100

    ajouter class (favorieExtendsAnnonce + supprimer Favories) dans model et dans favoriesServices j'ai creer 2 function ajouter_favorie et consulter_favorie et j'ai pratiquer dans la class main

--- src/services/FavoriesServices.java
+++ src/services/FavoriesServices.java
@@ -1,4 +1,77 @@
 package services;
 
+import database.DBConnection;
+import model.FaavoriesExtendAnnonces;
+
+import java.sql.*;
+import java.util.*;
+
 public class FavoriesServices {
+
+    private Connection con;
+    public FavoriesServices(){
+        this.con=DBConnection.getConnection();
+    }
+
+    public void ajouter_favorie(int Idannonces,int IdUser){
+
+
+        String Sql="INSERT INTO `favoris` (Date_ajout,id_annonce,id_user) VALUES (NOW(),?,?) ";
+
+        try(
+            PreparedStatement stmt=con.prepareStatement(Sql);)
+        {
+            stmt.setInt(1,Idannonces);
+            stmt.setInt(2,IdUser);
+            // 3. Execution
+            int rows = stmt.executeUpdate();
+            if (rows > 0) {
+                System.out.println("Succès: Annonce ajoutée aux favoris !");
+            }
+
+        }
+        catch (SQLException e){
+            System.out.println("Error Sql:"+e.getMessage());
+        }
+    }
+
+
+
+    public List<FaavoriesExtendAnnonces> Consulter_list_favorie(int idUser){
+        List<FaavoriesExtendAnnonces> list = new ArrayList<>();
+        String sql="SELECT a.*, f.* FROM annonce a INNER JOIN favoris f ON a.id_annonce = f.id_annonce WHERE f.id_user = ?; ";
+        try(PreparedStatement stmt=con.prepareStatement(sql)){
+
+            stmt.setInt(1,idUser);
+
+            ResultSet res=stmt.executeQuery();
+
+            while(res.next()){
+                FaavoriesExtendAnnonces fav=new FaavoriesExtendAnnonces(
+                        res.getInt("id_annonce"),
+                        res.getString("Titre"),
+                        res.getString("Description"),
+                        res.getDouble("Prix"),
+                        res.getString("Telephone"),
+                        res.getString("Type"),
+                        res.getTimestamp("Date_publication"),
+                        res.getInt("id_user"),
+                        res.getInt("id_ville"),
+                        res.getInt("id_categorie"),
+                        res.getTimestamp("Date_ajout"),
+                        res.getInt("id_favoris")
+                );
+                list.add(fav);
+            }
+
+
+
+        }catch (SQLException e){
+            System.out.println("Error Sql :"+e.getMessage());
+        }
+        return list;
+
+    }
+
+
 }
