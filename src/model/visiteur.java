591b91d3293ed41432066e45b990b632d82ad52c
Meryem26-a <lemrabetmeryem8@gmail.com>
2026-04-05 16:27:44 +0100

    visiteur

--- src/model/visiteur.java
+++ src/model/visiteur.java
@@ -1,4 +1,14 @@
 package model;
 
-public class visiteur {
+public class Visiteur extends User{
+    public Visiteur(int id,String Nom, String prenom, String gmail, String password,String telephone) {
+       super(id, Nom, prenom ,password, telephone);
+    }
+    public Visiteur(String Nom, String prenom, String gmail, String password, String telephone){
+        super(0,Nom, prenom, gmail, password, telephone);
+    }
+}
+public void s_inscrire(){
+    System.out.println("Le visiteur s'inscrit");
+
 }
