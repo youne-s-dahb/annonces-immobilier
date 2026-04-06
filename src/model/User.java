5456974e01a2415d86025cb4dfa895ec22e08d59
Youne-s-dahbi <dahyoubi123@gmail.com>
2026-04-07 00:03:18 +0100

    ajouter un role dans db donc je fais modification dans class admin et user et compte j'ai ajouter dans ces class attribut role et getrole et setrole et dans main j'ai ajouter menu admin

--- src/model/User.java
+++ src/model/User.java
@@ -2,11 +2,11 @@ package model;
 
 public class User extends compte{
 
-    public User(int id, String Nom, String prenom, String gmail, String password, String telephone) {
-        super(id, Nom, prenom, gmail, password, telephone);
+    public User(int id, String Nom, String prenom, String gmail, String password, String telephone,String role) {
+        super(id, Nom, prenom, gmail, password, telephone ,role);
     }
-    public User(String Nom, String prenom, String gmail, String password, String telephone) {
-        super(0,Nom, prenom, gmail, password, telephone);
+    public User(String Nom, String prenom, String gmail, String password, String telephone,String role) {
+        super(0,Nom, prenom, gmail, password, telephone,role);
     }
 
 }
